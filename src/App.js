b17e48eafd7b13c3e48594a93a2ef4df3763d8ec
ZaeemlAk <152154210+ZaeemlAk@users.noreply.github.com>
2024-12-01 00:47:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, { Suspense, useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 
-function App() {
+import './App.css';
+import loadingAnimation from './Assets/Images/LoadingAnimation.svg';
+import router from './Routes/Routes';
+import store from './app/store';
+import UserContext from './Context/UserContext';
+
+const App = () => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+      },
+    },
+  });
+
+  // card animator
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className=" h-full bg-white text-black m-[0_auto] w-[95%] max-w-[1440px] px-0 sm:w-[90%] md:w-[80%] xl:w-[75%]">
+      <QueryClientProvider client={client}>
+        <Suspense
+          fallback={
+            <div className="grid h-screen  place-items-center">
+              <img src={loadingAnimation} alt="" />
+            </div>
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {/* <UserContext> */}
+          <Provider store={store}>
+          <UserContext>
+            <RouterProvider router={router} />
+
+            <Toaster />
+            </UserContext>
+          </Provider>
+        </Suspense>
+      </QueryClientProvider>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import App from './App';
+import UserContext from './Context/UserContext';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+       
+            <App />
+            <ToastContainer autoClose={1000} />
+   
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
